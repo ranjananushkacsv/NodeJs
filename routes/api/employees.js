c127a04a0fd5566b5a89fef422747e428661a54f
ranjananushkacsv <ranjananushka90@gmail.com>
2025-04-13 10:21:18 +0530

    Updated till last class

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,23 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
+const express = require('express')
+const router = express.Router()
+const path = require('path')
 const empController = require('../../controllers/empController')
+const verifyJWT = require('../../middleware/verifyJWT')
 
-
-// Route for root path '/'
 router.route('/')
     .get(empController.getAllEmployees)
     .post(empController.createNewEmployee)
     .put(empController.updateEmployee)
-    .delete(empController.deleteEmployee);
+    .delete(empController.deleteEmployee)
 
-// Route for specific employee by ID
 router.route('/:id')
-    .get(empController.getEmployee);
+    .get(empController.getEmployee)
 
+module.exports = router
 
-module.exports = router;
-
-
-
-//MVC-model view controller: way to organize express app
+// MVC - Model View COntroller - way of organizing your express app
--- routes/root.js
+++ routes/root.js
@@ -1,23 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const empController = require('../../controllers/empController')
-
-
-// Route for root path '/'
-router.route('/')
-    .get(empController.getAllEmployees)
-    .post(empController.createNewEmployee)
-    .put(empController.updateEmployee)
-    .delete(empController.deleteEmployee);
-
-// Route for specific employee by ID
-router.route('/:id')
-    .get(empController.getEmployee);
-
-
-module.exports = router;
-
-
-
-//MVC-model view controller: way to organize express app
+const express = require('express')
+const router = express.Router()
+const path = require('path')
+// root route
+router.get(/^\/$|index(.html)?/, (req, res) => {
+    res.sendFile(path.join(__dirname,'..','views','index.html'));
+});
+module.exports = router
+  
